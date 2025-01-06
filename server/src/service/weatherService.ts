e2b2812d902118c3a15e6c53491856cc6081dd1b
achensen <Erinrjacobsen@gmail.com>
2025-01-06 00:00:42 -0500

    cleaned up weatherService comments

--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -1,91 +1,75 @@
 import dotenv from 'dotenv';
 dotenv.config();
-
-// TODO: Define an interface for the Coordinates object
-
-// TODO: Define a class for the Weather object
+//Creation of Weather class
 class Weather {
-  date:string;
-  icon:string;
-  iconDescription:string;
-  tempF:number;
-  windSpeed:number;
-  humidity:number;
-  city:string;
+  date: string;
+  icon: string;
+  iconDescription: string;
+  tempF: number;
+  windSpeed: number;
+  humidity: number;
+  city: string;
 
-  constructor (
-  date:string,
-  icon:string,
-  iconDescription:string,
-  tempF:number,
-  windSpeed:number,
-  humidity:number,
-  city:string,
+  constructor(
+    date: string,
+    icon: string,
+    iconDescription: string,
+    tempF: number,
+    windSpeed: number,
+    humidity: number,
+    city: string,
 
-  ){
-    this.date=date
-    this.icon=icon
-    this.iconDescription=iconDescription
-    this.tempF=tempF
-    this.windSpeed=windSpeed
-    this.humidity=humidity
-    this.city=city
+  ) {
+    this.date = date
+    this.icon = icon
+    this.iconDescription = iconDescription
+    this.tempF = tempF
+    this.windSpeed = windSpeed
+    this.humidity = humidity
+    this.city = city
   }
 }
-//Complete the WeatherService class
+// Creation of WeatherService Class
 class WeatherService {
-  // TODO: Define the baseURL, API key, and city name properties
+
   cityName: string
 
-  constructor (
-    cityName:string
+  constructor(
+    cityName: string
 
-  ){
-    this.cityName=cityName
+  ) {
+    this.cityName = cityName
   }
 
-
-
-  // TODO: Create fetchLocationData method
-  // private async fetchLocationData(query: string) {}
-  // TODO: Create destructureLocationData method
-  // private destructureLocationData(locationData: Coordinates): Coordinates {}
-  // TODO: Create buildGeocodeQuery method
-  // private buildGeocodeQuery(): string {}
-  // TODO: Create buildWeatherQuery method
-  // private buildWeatherQuery(coordinates: Coordinates): string {}
-  // TODO: Create fetchAndDestructureLocationData method
-  // private async fetchAndDestructureLocationData() {}
-  // TODO: Create fetchWeatherData method
-  // private async fetchWeatherData(coordinates: Coordinates) {}
-  // TODO: Build parseCurrentWeather method
-  // private parseCurrentWeather(response: any) {}
-  // TODO: Complete buildForecastArray method
-  // private buildForecastArray(currentWeather: Weather, weatherData: any[]) {}
-  // TODO: Complete getWeatherForCity method
-
+  //Uses getWeatherforCity to fetch the data for the correct city
   async getWeatherForCity(city: string) {
-    const weatherUrl= `${process.env.API_BASE_URL}/data/2.5/forecast?q=${city}&units=imperial&appid=${process.env.API_KEY}`
-    const response = await fetch (weatherUrl) 
-  
+    const weatherUrl = `${process.env.API_BASE_URL}/data/2.5/forecast?q=${city}&units=imperial&appid=${process.env.API_KEY}`
+    //fetches the data here
+    const response = await fetch(weatherUrl)
+    //once fetch is complete thanks to await, the response is populated
     const data = await response.json()
 
-    const filteredData = data.list.filter((_item:any,index:any)=>index===0 || index === 7 || index === 15 || index ===23 || index === 31 || index=== 39 )
-    console.log ('filtered data', filteredData)
+
+    //filter method is used with specific indexs to create a 5-day forecast
+    const filteredData = data.list.filter((_item: any, index: any) => index === 0 || index === 7 || index === 15 || index === 23 || index === 31 || index === 39)
+    console.log('filtered data', filteredData)
 
 
-
-    const restructeredData = filteredData.map((item:any)=>{
-      // const formattedDate= await this.formatDate(item.dt).toString()
-    const date = new Date(parseInt(item.dt) * 1000) 
-    const formattedDate =   date.toLocaleDateString('en-US', {  
-    weekday: 'long',
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  });
+    //Converts the date from the API into user friendly style 
+    const restructeredData = filteredData.map((item: any) => {
+      //dt is used by the API to provide date information in Unix timestamp format
+      //first convert to milliseconds
+      const date = new Date(parseInt(item.dt) * 1000)
+      //formatting 
+      const formattedDate = date.toLocaleDateString('en-US', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      });
       return new Weather(formattedDate, item.weather[0].icon, item.weather[0].description, item.main.temp, item.wind.speed, item.main.humidity, data.city.name)
     })
+    //return restructeredData so the fomattedDate and the weather forcast will both be present
     return restructeredData
   }
 }
